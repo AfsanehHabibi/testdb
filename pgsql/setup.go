96dd8b048061ee13abf71bae189c16a3ea04c3db
Afsaneh Habibi <a.habibi1394@gmail.com>
2024-04-29 08:59:20 -0400

    Add more configuration and execute function

--- pgsql/setup.go
+++ pgsql/setup.go
@@ -1,17 +1,45 @@
 package pgsql
 
 import (
+	"fmt"
 	"os/exec"
 )
+
+const (
+	CONTAINER_NAME = "test-postgres"
+	DEFAULT_IMAGE  = "postgres:latest"
+	DEFAULT_USER   = "root"
+	DEFAULT_DB     = "default"
+	DEFAULT_PORT   = 5432
+)
+
+type Config struct {
+	ImageName  string
+	DBName     string
+	DBUser     string
+	DBPassword string
+	PORT       int32
+}
 
 type TestPG struct {
 	containerID string
 }
 
-func (t *TestPG) Setup() error {
-	cmd := exec.Command("docker", "run", "-e", "POSTGRES_HOST_AUTH_METHOD=trust",
-		"-e", "POSTGRES_DB=broker", "-e", "POSTGRES_USER=admin",
-		"-d", "-p", "5432:5432", "--name", "test-postgres", "postgres:latest")
+func (t *TestPG) Setup(config *Config) error {
+	config = setDefaultConfigValues(config)
+	var passString string
+	if config.DBPassword == "" {
+		passString = "POSTGRES_HOST_AUTH_METHOD=trust"
+	} else {
+		passString = fmt.Sprintf("POSTGRES_PASSWORD=%s", config.DBPassword)
+	}
+
+	cmd := exec.Command("docker", "run",
+		"-e", fmt.Sprintf("POSTGRES_DB=%s", config.DBName),
+		"-e", fmt.Sprintf("POSTGRES_USER=%s", config.DBUser),
+		"-e", passString,
+		"-d", "-p", fmt.Sprintf("%d:5432", config.PORT),
+		"--name", CONTAINER_NAME, config.ImageName)
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -20,6 +48,29 @@ func (t *TestPG) Setup() error {
 
 	t.containerID = string(output)
 	return nil
+}
+
+func setDefaultConfigValues(config *Config) *Config {
+	if config == nil {
+		config = &Config{}
+	}
+
+	if config.ImageName == "" {
+		config.ImageName = DEFAULT_IMAGE
+	}
+
+	if config.DBUser == "" {
+		config.DBUser = DEFAULT_USER
+	}
+
+	if config.DBName == "" {
+		config.DBName = DEFAULT_DB
+	}
+
+	if config.PORT == 0 {
+		config.PORT = DEFAULT_PORT
+	}
+	return config
 }
 
 func (t *TestPG) TearDown() error {
@@ -36,7 +87,7 @@ func (t *TestPG) TearDown() error {
 }
 
 func (t *TestPG) stop() error {
-	cmd := exec.Command("docker", "stop", "test-postgres")
+	cmd := exec.Command("docker", "stop", CONTAINER_NAME)
 
 	_, err := cmd.CombinedOutput()
 	if err != nil {
@@ -46,11 +97,24 @@ func (t *TestPG) stop() error {
 }
 
 func (t *TestPG) remove() error {
-	cmd := exec.Command("docker", "rm", "test-postgres")
+	cmd := exec.Command("docker", "rm", CONTAINER_NAME)
 
 	_, err := cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
 	return nil
-} 
+}
+
+func (t *TestPG) Execute(query string) error {
+	cmd := exec.Command("docker", "exec", CONTAINER_NAME,
+		"psql", "-p", "5432", "-U", DEFAULT_USER, "-d", DEFAULT_DB, "-c", query)
+
+	output, err := cmd.CombinedOutput()
+	println(string(output))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- pgsql/setup_test.go
+++ pgsql/setup_test.go
@@ -7,18 +7,81 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWhenSettingUpNewContainerWithDefaultValueReturnNoError(t *testing.T) {
+func TestWhenSetupWithDefaultSetupAndTeardownReturnsNoError(t *testing.T) {
 	con := TestPG{}
-	err := con.Setup()
+	err := con.Setup(nil)
+	assert.NoError(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	err = con.TearDown()
 	assert.NoError(t, err)
 }
 
-func TestWhenTearingDownContainerReturnNoError(t *testing.T) {
+func TestWhenImageNameIsProvidedAndDoesNotExistReturnsError(t *testing.T) {
 	con := TestPG{}
-	err := con.Setup()
+	config := &Config{
+		ImageName: "non-existent:latest",
+	}
+	err := con.Setup(config)
+	assert.Error(t, err)
+}
+
+func TestWhenImageNameIsProvidedAndExistReturnsError(t *testing.T) {
+	con := TestPG{}
+	config := &Config{
+		ImageName: "postgres:latest",
+	}
+	err := con.Setup(config)
 	assert.NoError(t, err)
 
-	time.Sleep(10*time.Second)
+	time.Sleep(1 * time.Second)
+
+	err = con.TearDown()
+	assert.NoError(t, err)
+}
+
+func TestWhenExecutingQueryReturnsNoError(t *testing.T) {
+	con := TestPG{}
+	err := con.Setup(nil)
+	assert.NoError(t, err)
+
+	//Important! wait for database to setup
+	time.Sleep(3 * time.Second)
+	err = con.Execute("SELECT * FROM pg_catalog.pg_tables;")
+	assert.NoError(t, err)
+
+	err = con.TearDown()
+	assert.NoError(t, err)
+}
+
+func TestWhenDatabaseHasPasswordItExecutesQuery(t *testing.T) {
+	con := TestPG{}
+	config := &Config{
+		DBPassword: "pass",
+	}
+	err := con.Setup(config)
+	assert.NoError(t, err)
+
+	time.Sleep(3 * time.Second)
+	err = con.Execute("SELECT * FROM pg_catalog.pg_tables;")
+	assert.NoError(t, err)
+
+	err = con.TearDown()
+	assert.NoError(t, err)
+}
+
+func TestWhenPortIsProvidedItExecutesQuery(t *testing.T) {
+	con := TestPG{}
+	config := &Config{
+		PORT: 1234,
+	}
+	err := con.Setup(config)
+	assert.NoError(t, err)
+
+	time.Sleep(3 * time.Second)
+	err = con.Execute("SELECT * FROM pg_catalog.pg_tables;")
+	assert.NoError(t, err)
 
 	err = con.TearDown()
 	assert.NoError(t, err)
